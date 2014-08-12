0a7a45c2f6361e427052e371d830706085c75238
sardor <sardor@hanbiro.com>
2014-08-12 13:13:22 +0900

    More improvements

--- scraper.py
+++ scraper.py
@@ -2,23 +2,41 @@
 
 from bs4 import BeautifulSoup
 import urllib2
-import csv
+import string
 import re
 
-def get_names(letter):
-    page = urllib2.urlopen('http://ism.uz/letters/' + letter)
-    soup = BeautifulSoup(page.read())
-    men_names = soup.find_all('ol', class_='men-names')
-    names = []
-    for men_name in men_names:
-        names.append(men_name.text.strip().split())
+eng_alphabet = string.uppercase
+eng_alphabet = re.sub('W', '', eng_alphabet)
+eng_alphabet = re.sub('C', '', eng_alphabet)
+uzb_alphabet = [letter for letter in eng_alphabet] + ["O\xe2\x80\x98", "G\xe2\x80\x98", "SH", "CH"]
 
-    return names
+
+def get_men_names(letter):
+    page = urllib2.urlopen("http://ism.uz/letters/%s" % letter)
+    soup = BeautifulSoup(page.read())
+    names = soup.find_all('ol', class_='men-names')
+    names_array = []
+    for name in names:
+        names_array.append(name.text.strip().split())
+
+    return names_array[0]
+
+
+def get_women_names(letter):
+    page = urllib2.urlopen("http://ism.uz/letters/%s" % letter)
+    soup = BeautifulSoup(page.read())
+    names = soup.find_all('ol', class_='women-names')
+    names_array = []
+    for name in names:
+        names_array.append(name.text.strip().split())
+
+    return names_array[0]
 
 
 def main():
-    for a in get_names('B')[0]:
-        print a
+    for letter in ['A']:
+        print get_men_names(letter), len(get_men_names(letter))
+        print get_women_names(letter), len(get_women_names(letter))
 
 
 
