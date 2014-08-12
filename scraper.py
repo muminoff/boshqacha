c96725ef777d1529d8d64eb7258a448b1a4e3993
muminoff <muminoff@naver.com>
2014-08-12 22:44:25 +0900

    Sqlite support and more improvements

--- scraper.py
+++ scraper.py
@@ -4,39 +4,59 @@ from bs4 import BeautifulSoup
 import urllib2
 import string
 import re
+import sqlite3
 
 eng_alphabet = string.uppercase
 eng_alphabet = re.sub('W', '', eng_alphabet)
 eng_alphabet = re.sub('C', '', eng_alphabet)
 uzb_alphabet = [letter for letter in eng_alphabet] + ["O\xe2\x80\x98", "G\xe2\x80\x98", "SH", "CH"]
+urls = ["http://ism.uz/letters/" + letter for letter in uzb_alphabet]
+
+def _execute(query):
+    """Function to execute queries against a local sqlite database"""
+    dbPath = 'database.db'
+    connection = sqlite3.connect(dbPath)
+    cursorobj = connection.cursor()
+    try:
+            cursorobj.execute(query)
+            result = cursorobj.fetchall()
+            connection.commit()
+    except Exception:
+            raise
+    connection.close()
+    return result
 
 
-def get_men_names(letter):
-    page = urllib2.urlopen("http://ism.uz/letters/%s" % letter)
+def get_men_names(url):
+    page = urllib2.urlopen(url)
     soup = BeautifulSoup(page.read())
     names = soup.find_all('ol', class_='men-names')
     names_array = []
-    for name in names:
-        names_array.append(name.text.strip().split())
+    for names_ in names:
+        for name in names_.text.strip().split():
+            print name
+            _execute("INSERT INTO `ismlar` (`firstname`,`sex`) VALUES (\"{0}\", \"{1}\");".format(name.encode('utf-8'), "male"))
 
-    return names_array[0]
+    return
 
 
-def get_women_names(letter):
-    page = urllib2.urlopen("http://ism.uz/letters/%s" % letter)
+def get_women_names(url):
+    page = urllib2.urlopen(url)
     soup = BeautifulSoup(page.read())
     names = soup.find_all('ol', class_='women-names')
     names_array = []
-    for name in names:
-        names_array.append(name.text.strip().split())
+    for names_ in names:
+        for name in names_.text.strip().split():
+            print name
+            _execute("INSERT INTO `ismlar` (`firstname`,`sex`) VALUES (\"{0}\", \"{1}\");".format(name.encode('utf-8'), "female"))
 
-    return names_array[0]
-
+    return
 
 def main():
-    for letter in uzb_alphabet:
-        print get_men_names(letter), len(get_men_names(letter))
-        print get_women_names(letter), len(get_women_names(letter))
+
+    for url in urls:
+        get_women_names(url)
+        #print get_women_names(url)
 
 
 
---
+++ sql.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `ismlar` (
+      `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      `firstname` varchar(255) default NULL,
+      `sex` varchar(10) default NULL
+);
